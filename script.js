a2dbb1a58148f0c34257c95352cfd6321c8f20ed
Bahana Alana Khansa <alanabahana102@gmail.com>
2022-09-28 13:38:14 +0700

    fixed dino and bianglala at smaller screen size

--- script.js
+++ script.js
@@ -4,12 +4,39 @@ let dino = document.getElementById('dino');
 let gunungBack = document.getElementById('gunung-back');
 let header = document.querySelector('header');
 
-window.addEventListener('scroll', function() {
+const dinoLeft = 900;
+const bianglalaRight = 900;
+
+let value = window.scrollY;
+
+bianglala.style.transform = `translateX(-${value * 0.1}%)`;
+bianglala.style.right = bianglalaRight - ((bianglalaRight - window.innerWidth) * 0.3) + 'px';
+
+dino.style.transform = `translateX(${value * 0.1}%)`;
+dino.style.left = dinoLeft - ((dinoLeft - window.innerWidth) * 0.4) + 'px';
+
+logo.style.marginTop = value * 1 + 'px';
+header.style.top = value * 0.7 + 'px';
+gunungBack.style.marginTop = value * 0.4 + 'px';
+
+function addListenerMulti(element, eventNames, listener) {
+    var events = eventNames.split(' ');
+    for (var i=0, iLen=events.length; i<iLen; i++) {
+        element.addEventListener(events[i], listener, false);
+    }
+}
+
+addListenerMulti(window, 'scroll resize', function() {
     let value = window.scrollY;
 
+    bianglala.style.transform = `translateX(-${value * 0.1}%)`;
+    bianglala.style.right = bianglalaRight - ((bianglalaRight - window.innerWidth) * 0.3) + 'px';
+
+    dino.style.transform = `translateX(${value * 0.1}%)`;
+    dino.style.left = dinoLeft - ((dinoLeft - window.innerWidth) * 0.4) + 'px';
+
     logo.style.marginTop = value * 1 + 'px';
-    bianglala.style.transform = `translate(-${value * 0.1}%, 0%)`;
-    dino.style.transform = `translate(${value * 0.1 + 30}%, 0%)`;
     header.style.top = value * 0.7 + 'px';
     gunungBack.style.marginTop = value * 0.4 + 'px';
+    // console.log(window.innerWidth);
 });
