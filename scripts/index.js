fe293688d1d8240480abeed63c7a6e3c08ad7c23
Bahana Alana Khansa <alanabahana102@gmail.com>
2022-12-20 19:57:27 +0700

    mobile interactivity

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,3 @@
-// let logo = document.getElementById('logo-enggal');
-// let bianglala = document.getElementById('bianglala');
-// let dino = document.getElementById('dino');
-// let gunungBack = document.getElementById('gunung-back');
-
 let header = document.querySelector('header');
 let ourProject = document.querySelector('.sec .our-projects');
 let projectItem = document.querySelectorAll('.sec .our-projects div .project-item');
@@ -12,24 +7,17 @@ projectItem.forEach((val, i) => {
     val.classList.add('reverse');
 });
 
-// const dinoLeft = 900;
-// const bianglalaRight = 1000;
+let f = function () {
+    if (window.scrollY - window.innerHeight > 0) {
+        header.classList.add('header-background')
+    } else {
+        header.classList.remove('header-background')
+    }
+}
+f();
 
-// let f = function () {
-//     let value = window.scrollY;
-    
-//     bianglala.style.transform = `translateX(-${value * 0.1}%)`;
-//     bianglala.style.right = bianglalaRight - ((bianglalaRight - window.innerWidth) * 0.3) + 'px';
-    
-//     dino.style.transform = `translateX(${value * 0.1}%)`;
-//     dino.style.left = dinoLeft - ((dinoLeft - window.innerWidth) * 0.4) + 'px';
-    
-//     gunungBack.style.marginTop = value * 0.4 + 'px';
-// }
-// f();
+function addListenerMulti(element, eventNames, listener) {
+    eventNames.split(' ').forEach(item => element.addEventListener(item, listener, false));
+}
 
-// function addListenerMulti(element, eventNames, listener) {
-//     eventNames.split(' ').forEach(item => element.addEventListener(item, listener, false));
-// }
-
-// addListenerMulti(window, 'scroll resize', f);
+addListenerMulti(window, 'scroll resize', f);
