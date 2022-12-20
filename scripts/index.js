8e59d3beeb7fd9343609e654acf689b49217c446
Bahana Alana Khansa <alanabahana102@gmail.com>
2022-12-20 14:35:19 +0700

    removed placeholder logo, changed background, fixed description overflow

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
-let logo = document.getElementById('logo-enggal');
-let bianglala = document.getElementById('bianglala');
-let dino = document.getElementById('dino');
-let gunungBack = document.getElementById('gunung-back');
+// let logo = document.getElementById('logo-enggal');
+// let bianglala = document.getElementById('bianglala');
+// let dino = document.getElementById('dino');
+// let gunungBack = document.getElementById('gunung-back');
+
 let header = document.querySelector('header');
 let ourProject = document.querySelector('.sec .our-projects');
 let projectItem = document.querySelectorAll('.sec .our-projects div .project-item');
@@ -11,24 +12,24 @@ projectItem.forEach((val, i) => {
     val.classList.add('reverse');
 });
 
-const dinoLeft = 900;
-const bianglalaRight = 1000;
+// const dinoLeft = 900;
+// const bianglalaRight = 1000;
 
-let f = function () {
-    let value = window.scrollY;
+// let f = function () {
+//     let value = window.scrollY;
     
-    bianglala.style.transform = `translateX(-${value * 0.1}%)`;
-    bianglala.style.right = bianglalaRight - ((bianglalaRight - window.innerWidth) * 0.3) + 'px';
+//     bianglala.style.transform = `translateX(-${value * 0.1}%)`;
+//     bianglala.style.right = bianglalaRight - ((bianglalaRight - window.innerWidth) * 0.3) + 'px';
     
-    dino.style.transform = `translateX(${value * 0.1}%)`;
-    dino.style.left = dinoLeft - ((dinoLeft - window.innerWidth) * 0.4) + 'px';
+//     dino.style.transform = `translateX(${value * 0.1}%)`;
+//     dino.style.left = dinoLeft - ((dinoLeft - window.innerWidth) * 0.4) + 'px';
     
-    gunungBack.style.marginTop = value * 0.4 + 'px';
-}
-f();
+//     gunungBack.style.marginTop = value * 0.4 + 'px';
+// }
+// f();
 
-function addListenerMulti(element, eventNames, listener) {
-    eventNames.split(' ').forEach(item => element.addEventListener(item, listener, false));
-}
+// function addListenerMulti(element, eventNames, listener) {
+//     eventNames.split(' ').forEach(item => element.addEventListener(item, listener, false));
+// }
 
-addListenerMulti(window, 'scroll resize', f);
+// addListenerMulti(window, 'scroll resize', f);
